47cb965f133c197c898652dcdf629286047617ba
KeeganMcGinnis2 <keeganmcginnis219@gmail.com>
2019-07-31 22:11:39 -0700

    leg

--- connect4.cpp
+++ connect4.cpp
@@ -2,34 +2,118 @@
 #include "players.h"
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     connect4Board game_board;
-    //Human player("ryan.py");
-    Computer player("AI", 1000, true);
-    Computer AI("AI", 1000, false);
+    int player1, player2, heuristic;
+    Human hp1, hp2;
+    Computer cp1, cp2;
+    bool human1, human2;
+    
+    cout << "Please enter 1 if player 1 should be human, and 2 if player 1 should be an AI:\n";
+    cin >> player1;
+    while(player1 != 1 && player1 != 2) {
+         cout << "Please enter 1 if player 1 should be human, and 2 if player 1 should be an AI:\n";
+         cin >> player1;
+    }
+
+    if(player1 == 1) {
+        Human hp1("p1");
+        human1 = true;
+    }
+
+    else {
+        cout << "Please enter 1 if player 1 should use a heuristic, and 2 if it should not:\n";
+        cin >> heuristic;
+        
+        while(heuristic != 1 && heuristic != 2) {
+            cout << "Please enter 1 if player 1 should use a heuristic, and 2 if it should not:\n";
+            cin >> heuristic;
+        }
+
+        if(heuristic == 1) {
+            Computer cp1("Heuristic1", 1000, true);
+            human1 = false;
+        }
+        
+        else {
+            Computer cp1("Pure1", 1000, false);
+            human1 = false;
+        }
+    }
+
+    cout << "Please enter 1 if player 2 should be human, and 2 if player 2 should be an AI:\n";
+    cin >> player2;
+    while(player2 != 1 && player2 != 2) {
+         cout << "Please enter 1 if player 2 should be human, and 2 if player 2 should be an AI:\n";
+         cin >> player2;
+    }
+
+    if(player2 == 1) {
+        Human hp2("p2");
+        human2 = true;
+    }
+
+    else {
+        cout << "Please enter 1 if player 2 should use a heuristic, and 2 if it should not:\n";
+        cin >> heuristic;
+        
+        while(heuristic != 1 && heuristic != 2) {
+            cout << "Please enter 1 if player 2 should use a heuristic, and 2 if it should not:\n";
+            cin >> heuristic;
+        }
+
+        if(heuristic == 1) { 
+            Computer cp2("Heuristic1", 1000, true);
+            human2 = false;
+        }
+        
+        else {
+            Computer cp2("Pure1", 1000, false);
+            human2 = false;
+        }
+    }
+    
+    
     int index;
+    cout << game_board << "\n";
     while(!game_board.isFull()) {
-        index = player.chooseMove(game_board);
+        if(human1 == true) 
+            index = hp1.chooseMove(game_board);
+        else
+            index = cp1.chooseMove(game_board);
+
         while(game_board.columnFull(index)) {
             cout << "column is full, pick another column\n";
-            index = player.chooseMove(game_board);
+            if(human1 == true)
+                index = hp1.chooseMove(game_board);
+            else
+                index = cp1.chooseMove(game_board);
         }
         game_board.setIndex(index);
         if(game_board.checkWin() || game_board.isFull()) {
+            cout << "p1 wins!!!\n";
             break;
         }
         cout << game_board << "\n";
         game_board.swapTurn();
-        index = AI.chooseMove(game_board);
+        if(human2 == true)
+            index = hp2.chooseMove(game_board);
+        else
+            index = cp2.chooseMove(game_board);
         while(game_board.columnFull(index)) {
             cout << "column is full, pick another column\n";
-            index = AI.chooseMove(game_board);
+            if(human2 == true)
+            index = hp2.chooseMove(game_board);
+        else
+            index = cp2.chooseMove(game_board);
         }
         game_board.setIndex(index);
         if(game_board.checkWin() || game_board.isFull()) {
+            cout << "p2 wins!!!\n";
             break;
         }
         cout << game_board << "\n";
@@ -37,6 +121,6 @@ int main() {
 
     }
 
-    cout << game_board << "\n";
+    cout << "\n" << game_board << "\n";
     return 0;
 }
